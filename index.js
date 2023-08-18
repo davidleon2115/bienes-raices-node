bac4421c0bd9c15b331f5178c74fa53d9b8d0f00
David Leon <leon95024@gmail.com>
2023-08-18 02:04:00 -0500

    enabled ECM

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hola Mundo");
+import express from "express";
+
+//Crear la app
+const app = express();
+
+//Routing
+app.get('/', (req, res) => {
+        res.send("Hola Mundo en express");
+    })
+
+app.get('/nosotros', function(req, res){
+    res.send("Información de nosotros")
+})
+
+//Definir un puerto y arrancar el proyecto
+const port = 3000;
+
+app.listen(port, ()=>{
+    console.log(`El servidor está funcionando en el puerto ${port}`)
+})
