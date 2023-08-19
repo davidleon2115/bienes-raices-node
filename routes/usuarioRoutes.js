5986df900bf10ed57b85e80835a86861c009a22c
David Leon <leon95024@gmail.com>
2023-08-19 17:09:23 -0500

    Added: view registro.pug

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -1,10 +1,10 @@
 import express from "express";
+import { formularioLogin, formularioRegistro } from "../controllers/usuarioController.js";
 
 const router = express.Router();
 
 
-router.get('/login', (req, res) => {
-    res.render('auth/login');
-})
+router.get('/login', formularioLogin)
+router.get('/registro', formularioRegistro)
 
 export default router;
