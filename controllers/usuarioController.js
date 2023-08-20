16b1f5ae9264b6fbec6a431f702f30d281ccd0b7
David Leon <leon95024@gmail.com>
2023-08-20 00:58:59 -0500

    Se añade formulario a registro.pug

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,12 +1,12 @@
 const formularioLogin = (req, res) => {
     res.render('auth/login', {
-
+        pagina: 'Login'
     });
 }
 
 const formularioRegistro = (req, res) => {
     res.render('auth/registro', {
-
+        pagina: 'Crear Cuenta'
     })
 }
 
