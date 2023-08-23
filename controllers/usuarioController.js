09927d3b585493499294927d5fe67f1e9186ecfa
David Leon <leon95024@gmail.com>
2023-08-23 01:12:48 -0500

    Se agregan validaciones

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,5 @@
+import { check, validationResult } from 'express-validator'
+
 import Usuario from "../models/Usuario.js";
 
 
@@ -15,6 +17,23 @@ const formularioRegistro = (req, res) => {
 }
 
 const registrar = async (req, res) => {
+    //validación
+    await check('nombre').notEmpty().withMessage('El nombre es obligatorio').run(req)
+    await check('email').isEmail().withMessage('Eso no parece un email').run(req)
+    await check('password').isLength({min: 6}).withMessage('la contraseña debe ser de almenos 6 caracteres').run(req)
+    await check('repetir_password').equals('password').withMessage('Las contraseñas no coinciden').run(req)
+    
+    let resultado = validationResult(req)
+
+    //Verificar que el resultado esté vacio
+    if(!resultado.isEmpty()){
+        //Errores
+        return res.render('auth/registro', {
+            pagina: 'Crear Cuenta',
+            errores: resultado.array()
+        })
+    }
+
     const usuario = await Usuario.create(req.body)
     res.json(usuario)
 }
