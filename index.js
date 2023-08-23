3e31171d04cbf90470763e658b76f6ec0887adc7
David Leon <leon95024@gmail.com>
2023-08-23 00:24:09 -0500

    Se agrega urlencoded

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import usuarioRoutes from "./routes/usuarioRoutes.js";
 import db from "./config/db.js";
 
 //Crear la app
 const app = express();
+
+//Habilitar lectura de datos de formularios
+app.use( urlencoded({extended: true}) )
 
 //Conexion a la base de datos
 try {
